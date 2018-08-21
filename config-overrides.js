40909b550e20ceecd323ac45edc70e32c80e08ec
沈经纬 <327538014@qq.com>
2018-08-21 11:35:48 +0800

    fix: 修复开发环境时，publicPath错误的问题

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config.output.publicPath = './'; // 设置输出的公共路径为./
+  if (env === "production") {
+    config.output.publicPath = './'; // 设置输出的公共路径为./
+  }
   return config;
 };
